08c6edcc0b584c045ef10641003c9f6d28359449
ĐỨC MẠNH <ducmanh27101994@gmail.com>
2020-06-14 09:16:21 +0700

    viết hàm main để clear ảnh và tạo di chuyển cho ảnh

--- enemy.js
+++ enemy.js
@@ -1,52 +1,29 @@
 class Enemy {
-    constructor(a,b,speed) {
-        this.a = a;
-        this.b = b;
+    constructor(x, y, speed) {
+        this.x = x;
+        this.y = y;
         this.speed = speed;
     }
-    // creatEnemy() {
-    //     let myCanvas = document.getElementById('myCanvas');
-    //     let ctx = myCanvas.getContext('2d');
-    //     let img = new Image;
-    //     img.onload = function () {
-    //         ctx.drawImage(img, 100, 200);
-    //     };
-    //     img.src = "image/stone.png";
-    // }
 
-    move() {
-        this.y += this.speed;
-    }
+    // creatEnemy() {
+//     //     let myCanvas = document.getElementById('myCanvas');
+//     //     let ctx = myCanvas.getContext('2d');
+//     //     let img = new Image;
+//     //     img.onload = function () {
+//     //         ctx.drawImage(img, 100, 200);
+//     //     };
+//     //     img.src = "image/stone.png";
+//     // }
+
     draw(canvas) {
         let img = document.getElementById('img1');
         let pen = canvas.getContext('2d');
         pen.beginPath();
-        pen.drawImage(img, this.a, this.b, 50, 50);
+        pen.drawImage(img, this.x, this.y, 50, 50);
         pen.fill();
     }
-}
 
-function createEnemy() {
-    let speed = Math.floor(Math.random() * 5 + 5);
-    let x = Math.random() * window.innerWidth;
-    let y = 0;
-
-    let enemy = new Enemy(x, y, speed);
-    enemy.draw(canvas);
-    enemys.push(enemy);
-}
-
-function main() {
-    pen.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < enemys.length; i++) {
-        enemy[i].move();
-        enemys[i].draw(canvas);
-    }
-    requestAnimationFrame(main);
-}
-
-function createMultiStone() {
-    for (let i = 0; i < 10; i++) {
-        createStone();
+    move() {
+        this.y += this.speed;
     }
 }
