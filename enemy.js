e4752bae1dec15960459680c0dc532ab027f59db
ĐỨC MẠNH <ducmanh27101994@gmail.com>
2020-06-14 08:46:00 +0700

    tao hàm hiển thị enemy

--- enemy.js
+++ enemy.js
@@ -4,17 +4,24 @@ class Enemy {
         this.b = b;
         this.speed = speed;
     }
-    creatEnemy() {
-        let myCanvas = document.getElementById('myCanvas');
-        let ctx = myCanvas.getContext('2d');
-        let img = new Image;
-        img.onload = function () {
-            ctx.drawImage(img, 100, 200);
-        };
-        img.src = "image/stone.png";
+    // creatEnemy() {
+    //     let myCanvas = document.getElementById('myCanvas');
+    //     let ctx = myCanvas.getContext('2d');
+    //     let img = new Image;
+    //     img.onload = function () {
+    //         ctx.drawImage(img, 100, 200);
+    //     };
+    //     img.src = "image/stone.png";
+    // }
 
-    }
     move() {
         this.y += this.speed;
+    }
+    draw(canvas) {
+        let img = document.getElementById('img1');
+        let pen = canvas.getContext('2d');
+        pen.beginPath();
+        pen.drawImage(img, this.a, this.b, 50, 50);
+        pen.fill();
     }
 }
